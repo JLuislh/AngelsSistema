5195d68516354e032490482c2800501603f79680
jluis <jluis@TI.Anatekelectronics.local>
2024-03-12 20:55:45 -0600

    Logica de menus y finalizacion de orden

--- src/angels/Tiket.java
+++ src/angels/Tiket.java
@@ -3,14 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package angels;
+import ClassAngels.EtiquetasClass;
+import ClassAngels.TiketsImpresion;
 import br.com.adilson.util.Extenso;
 import br.com.adilson.util.PrinterMatrix;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 import javax.print.Doc;
 import javax.print.DocFlavor;
 import javax.print.DocPrintJob;
@@ -21,7 +21,6 @@ import javax.print.SimpleDoc;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
 
 /**
  *
@@ -33,77 +32,78 @@ public class Tiket {
     
     public static void main(String[] args) throws IOException { 
     
-    String subTotal = "";
-    String total= "";
-    String dineroR= "";
-    String devolucion= "";
-    //JTable jtbl_venta); 
+    String subTotal = "25.00";
+    String total= "25.00";
+    String dineroR= "1000";
+    String devolucion= "10";
 
     
     PrinterMatrix printer = new PrinterMatrix();
-    Extenso e = new Extenso();
     
-    e.setNumber(20.30);
     
     //Definir el tamanho del papel para la impresion de dinamico y 32 columnas
-    int filas = 10;
+     ArrayList<TiketsImpresion> resultFilas = TiketsImpresion.ListaProductosTIket();
+    int filas = resultFilas.size();
     int tamaño = filas+40;
+    int p=0;
     printer.setOutSize(tamaño, 80);
         System.out.println("ENTRA");
     //Imprimir = 1ra linea de la columa de 1 a 32
-    printer.printTextWrap(0, 1, 5, 80, "===================================================================");
-    printer.printTextWrap(1, 1, 35, 80, "CARNIVOROS"); //Nombre establecimiento
-    printer.printTextWrap(3, 1, 31, 80, "Barrio Bellavista"); //Barrio
-    printer.printTextWrap(4, 1, 37, 80, "Clle 47"); //Direccion
-    printer.printTextWrap(5, 1, 38, 80, "12345"); //Codigo Postal
-    printer.printTextWrap(6, 1, 10, 41, "Fecha"); //Aqui va la fecha de recibo
-    printer.printTextWrap(6, 1, 38, 80, "Hora"); //Aqui va la hora de recibo
-    printer.printTextWrap(7, 1, 3, 80, "Numero"); //Numero del recibo - FACTURA O PEDIDO
-    printer.printTextWrap(8, 1, 3, 80, "Atiende"); //Nombre Cajero
-    printer.printTextWrap(9, 1, 3, 80, "Cliente");//Nombre del Cliente
-    printer.printTextWrap(10,1, 5, 80, "——————————–——————————–——————————–——————————–——–———–——–—–———–——–———");
-    printer.printTextWrap(11,1, 7, 80, "ID   DESCRIPCION                        CANT           PRECIO");
+    printer.printTextWrap(0, 1, 4, 80, "==========================================================");
+    printer.printTextWrap(1, 1, 25, 80, "ANGEL'S"); //Nombre establecimiento
+    printer.printTextWrap(3, 1, 25, 80, "LOCAL 13"); //Barrio
+    printer.printTextWrap(4, 1, 23, 80, "EL ENCUENTRO"); //Direccion
+    printer.printTextWrap(5, 1, 26, 80, "123456"); //Codigo Postal
+    printer.printTextWrap(6, 1, 4, 41, "Fecha"); //Aqui va la fecha de recibo
+    printer.printTextWrap(6, 1, 4, 80, "Hora"); //Aqui va la hora de recibo
+    printer.printTextWrap(7, 1, 23, 80, "AUTORIZACION"); //Numero del recibo - FACTURA O PEDIDO
+    printer.printTextWrap(8, 1, 24, 80, "SERIE"); //Nombre Cajero
+    printer.printTextWrap(9, 1, 22, 80, "NUMERO DE DTE");//Nombre del Cliente
+    printer.printTextWrap(10,1, 4, 80, "——————————–——————————–——————————–—–———");
+    printer.printTextWrap(11,1, 4, 80, "CANT           DESCRIPCION              PRECIO     TOTAL");
     printer.printTextWrap(12,1, 0, 80, " ");
-
-    /*for (int i = 0; i < filas; i++) {
-        //int p = 13+i; //Fila
-
-       /* printer.printTextWrap(p , 1, 7, 19 , jtbl_venta.getValueAt(i,0).toString());
-        printer.printTextWrap(p , 1, 12, 42 , jtbl_venta.getValueAt(i,1).toString());
-        printer.printTextWrap(p , 1, 47, 49, jtbl_venta.getValueAt(i,2).toString());
-
-        String pre= printer.alinharADireita(10, jtbl_venta.getValueAt(i,3).toString());
-        printer.printTextWrap(p , 1, 57, 80, pre);
-
-        //String inp= printer.alinharADireita(7,punto_Venta.jtbl_venta.getValueAt(i,6).toString());
-        //printer.printTextWrap(p , 1, 25, 32, inp);
-    }
-    DecimalFormat formateador = new DecimalFormat("#.###");*/
     
+    ArrayList<TiketsImpresion> result = TiketsImpresion.ListaProductosTIket();
+            for (int i = 0; i < result.size(); i++) {
+                 p = 13+i;
+            printer.printTextWrap(p , 1, 5, 8 , result.get(i).getCantidad());
+            printer.printTextWrap(p , 1, 9, 11 ,"|");
+            printer.printTextWrap(p , 1, 12, 39 , result.get(i).getDescripcion2());
+            printer.printTextWrap(p , 1, 41, 43 ,"|");
+            printer.printTextWrap(p , 1, 45, 50 , result.get(i).getPrecio());
+            printer.printTextWrap(p , 1, 51, 53 ,"|");
+            printer.printTextWrap(p , 1, 55, 60 , result.get(i).getTotal());
+          }
+            filas = p;
+            System.out.println("FILA "+p);
+        
+   
+   // DecimalFormat formateador = new DecimalFormat("#.###");
+    printer.printTextWrap(filas+1, 1, 4, 80, "==========================================================");
     String sub= printer.alinharADireita(10, subTotal);
-    printer.printTextWrap(filas+17, 1, 5, 80, "Subtotal: ");
-    printer.printTextWrap(filas+17, 1, 20, 80, "$"+sub);
+    printer.printTextWrap(filas+2, 1, 5, 80, "Subtotal: ");
+    printer.printTextWrap(filas+2, 1, 20, 80, "$"+sub);
 
     String tot= printer.alinharADireita(10, total);
-    printer.printTextWrap(filas+18, 1, 5, 80, "Total a pagar: ");
-    printer.printTextWrap(filas+18, 1, 20, 80, "$"+tot);
+    printer.printTextWrap(filas+3, 1, 5, 80, "Total a pagar: ");
+    printer.printTextWrap(filas+3, 1, 20, 80, "$"+tot);
 
     String efe= printer.alinharADireita(10,dineroR);
-    printer.printTextWrap(filas+19, 1, 5, 80, "Efectivo : ");
-    printer.printTextWrap(filas+19, 1, 20, 80, "$"+efe);
+    printer.printTextWrap(filas+4, 1, 5, 80, "Efectivo : ");
+    printer.printTextWrap(filas+4, 1, 20, 80, "$"+efe);
 
     String cam= printer.alinharADireita(10,devolucion);
-    printer.printTextWrap(filas+20, 1, 5, 80, "Cambio : ");
-    printer.printTextWrap(filas+20, 1, 20, 80, "$"+ cam);
+    printer.printTextWrap(filas+5, 1, 5, 80, "Cambio : ");
+    printer.printTextWrap(filas+5, 1, 20, 80, "$"+ cam);
 
-    printer.printTextWrap(filas+21, 1, 5, 80, "——————————–——————————–——————————–——————————–——–———–——–—–———–——–———");
-    printer.printTextWrap(filas+22, 1, 26,80, "!Gracias por su preferencia!");
-    printer.printTextWrap(filas+23, 1, 30, 80, "WorkIt App - v1.0.0");
-    printer.printTextWrap(filas+24, 1, 31, 80, "Software a Medida");
-    printer.printTextWrap(filas+25, 1, 25, 80, "Contacto: workitapp@gmail.com");
-    printer.printTextWrap(filas+26, 1, 5,80, "===================================================================");
+    printer.printTextWrap(filas+6, 1, 5, 80, "——————————–——————————–——————————–—–———");
+    printer.printTextWrap(filas+7, 1, 26,80, "!Gracias por su preferencia!");
+    printer.printTextWrap(filas+8, 1, 30, 80, "WorkIt App - v1.0.0");
+    printer.printTextWrap(filas+9, 1, 31, 80, "Software a Medida");
+    printer.printTextWrap(filas+10, 1, 25, 80, "Contacto: workitapp@gmail.com");
+    printer.printTextWrap(filas+11, 1, 5,80, "==========================================================");
 
-
+    
     ///CREAR ARCHIVO EN CARPETA DEL PROYECTO PARA PEDIDOS
     printer.toFile("C:\\tmp\\impresion.txt");
     FileInputStream inputStream = null;
