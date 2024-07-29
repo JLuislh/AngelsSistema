ce4da2c1fda54049edc72810a0fff6c675ab86ba
Jorge Lopez <Jorge Lopez@JorgeL>
2024-07-29 06:56:15 -0600

    Cambios Angels Paraiso

--- src/AdministradorAngels/AdTotalEnCajaParaiso.java
+++ src/AdministradorAngels/AdTotalEnCajaParaiso.java
@@ -231,6 +231,7 @@ public class AdTotalEnCajaParaiso extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton1ActionPerformed
   
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
         ValidarOrdenes();
         generarFechas();
         if(Fe.getDate() != null){ 
